cea8674f407eb8160e4f5c5b4d875aa576512556
yango3 <o3.yang@hotmail.com>
2021-08-26 15:28:57 +0800

    update init-mongo.js

--- init-mongo.js
+++ init-mongo.js
@@ -1,5 +1,12 @@
+print('Start ##########################');
+
+db.auth('john','dianke123');
+
+db = db.getSiblingDB('db');
+
+
 db.createUser({
-    user: "john",
+    user: "user001",
     pwd: "dianke001",
     roles:[
 	{
@@ -8,3 +15,19 @@ db.createUser({
 	}
    ]
 });
+
+db.article.drop();
+
+db.article.save( {
+    title : "this is my title" , 
+    author : "bob" , 
+    posted : new Date(1079895594000) , 
+    pageViews : 5 , 
+    tags : [ "fun" , "good" , "fun" ] ,
+    comments : [ 
+        { author :"joe" , text : "this is cool" } , 
+        { author :"sam" , text : "this is bad" } 
+    ],
+    other : { foo : 5 }
+});
+
