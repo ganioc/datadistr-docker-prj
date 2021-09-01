ecaa56d91b9916871408a15b0ac0adff3be724c9
yango3 <o3.yang@hotmail.com>
2021-09-01 14:41:24 +0800

    update file-uploader

--- file-uploader/src/app.module.ts
+++ file-uploader/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const HOST = process.env.HOST ? process.env.HOST : '192.168.0.199';
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5432;
+const USERNAME = process.env.USERNAME ? process.env.USERNAME : 'john';
+const DBNAME = process.env.PASSWORD ? process.env.PASSWORD : 'db';
+const PASSWORD = process.env.PASSWORD ? process.env.PASSWORD : 'dianke123';
+
 @Module({
-    imports: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: HOST,
+            port: PORT,
+            username: USERNAME,
+            password: PASSWORD,
+            database: DBNAME,
+            synchronize: true,
+            logging: true,
+            entities: ['dist/entity/**/*.js'],
+            migrations: ['build/migration/**/*.js'],
+            subscribers: ['build/subscriber/**/*.js'],
+            cli: {
+                entitiesDir: 'src/entity',
+                migrationsDir: 'src/migration',
+                subscribersDir: 'src/subscriber',
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private connection: Connection) { }
+}
